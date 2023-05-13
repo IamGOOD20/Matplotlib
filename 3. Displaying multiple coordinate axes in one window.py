f30110aec309c16f891243b6a2b961cf4660f6b1
IamGOOD <rukshenas1990@gmail.com>
2023-05-13 14:33:08 -0400

    Display multiple coordinate axes in one window: part 2 : GridSpec class

--- 3. Displaying multiple coordinate axes in one window.py
+++ 3. Displaying multiple coordinate axes in one window.py
@@ -93,9 +93,32 @@ matplotlib.use('Qt5Agg')
 # plt.show()
 
 # 4
-f, ax = plt.subplots()
-f.set_size_inches(7, 4)     # size
-f.set_facecolor('#eee')     # color
-ax.plot(np.arange(0, 5, 0.2))
-plt.show()
+# f, ax = plt.subplots()
+# f.set_size_inches(7, 4)     # size
+# f.set_facecolor('#eee')     # color
+# ax.plot(np.arange(0, 5, 0.2))
+# plt.show()
+# 
 
+# GridSpec - used for more complex chart layout
+# gs = GridSpec(ncols=3, nrows=2, figure=fig)
+
+# 1
+from matplotlib.gridspec import GridSpec
+
+ws = [1, 2, 5] # size
+hs = [2, 0.5] # size
+
+fig = plt.figure(figsize=(7, 4))
+gs = GridSpec(ncols=3, nrows=2, figure=fig, width_ratios=ws, height_ratios=hs)
+
+ax1 = plt.subplot(gs[0, 0])
+ax1.plot(np.arange(0, 5, 0.2))
+ax2 = fig.add_subplot(gs[1, 0:2])
+ax2.plot(np.random.random(10))
+ax3 = fig.add_subplot(gs[:, 2])
+ax3.plot(np.random.random(10))
+
+
+plt.show()
+# displays 3 graphs in one figure, simple
